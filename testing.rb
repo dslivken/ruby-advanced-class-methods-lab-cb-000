5b3c09b2481e3ef73f8bcb2b98b464cc70256530
David Slivken <slivken@gmail.com>
2018-06-21 19:34:06 +0000

    automatically backed up by learn

---
+++ testing.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist_name
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+end
+
+
+
+binding.pry
